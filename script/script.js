d85dbf8765b10dda5a1fb3a446dc7a829db0a533
BegaiAs <begaiasanbekova@gmail.com>
2021-02-02 16:22:30 +0100

    add string.trim()

--- script/script.js
+++ script/script.js
@@ -4,9 +4,10 @@ const isString = function (string) {
     if (typeof string !== 'string'){
         return ("B качестве аргумента передана не строка");
     }else if (string.length > 30){
+        string = string.trim();
         return string.slice(0, 30) + '...';
     }
 }
 
-console.log("Test 1: ", isString("Если строка более 30 знаков - то после 30го символа часть текста скрывается и вместо них появляются три точки (...)"));
-console.log("Test 2: ", isString(50));
+console.log("Test 1:", isString("             Если строка более 30 знаков - то после 30го символа часть текста скрывается и вместо них появляются три точки (...)           "));
+console.log("Test 2:", isString(50));
