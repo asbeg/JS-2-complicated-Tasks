9c9a29f66a555a099466f09296e218152141a2f1
BegaiAs <begaiasanbekova@gmail.com>
2021-02-04 20:31:45 +0100

    Lesson05 for loop Numbers

--- script/script.js
+++ script/script.js
@@ -1,15 +1,32 @@
-'use strict'
+'use strict';
+// 1
+const arr = ['556', '424', '242', '565', '789', '987', '255'];
 
-const isString = function (string) {
-    if (typeof string !== 'string') {
-        return ("B качестве аргумента передана не строка");
-    }
-
-    string = string.trim();
-    if (string.length > 30) {
-        return string.slice(0, 30) + '...';
+function searchNumber(item) {
+    //indexOf если знач не найдено возвр -1
+    if (item.indexOf('2') === 0 || item.indexOf('4') === 0) {
+        return item;
     }
 }
 
-console.log("Test 1:", isString("             Если строка более 30 знаков - то после 30го символа часть текста скрывается и вместо них появляются три точки (...)           "));
-console.log("Test 2:", isString(50));
+const startsWith = arr.filter(searchNumber);
+
+console.log(startsWith);
+
+// 2
+
+for (let i = 1; i <= 100; i++) {
+    let a = 0;
+//влож цикл
+    for (let j = 2; j < i; j++) {
+        if (i % j === 0) {
+            //если число не простое
+            //остльные - простые(а===0)
+            a = 1;
+            break;
+        }
+    }
+    if (i > 1 && a === 0) {
+        console.log("Делители этого числа: 1 и ", i);
+    }
+}
