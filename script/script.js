dc2a2850a874bf0b77a934c2c93368a8638422a0
BegaiAs <begaiasanbekova@gmail.com>
2021-02-22 21:07:10 +0100

    fix setInterval

--- script/script.js
+++ script/script.js
@@ -1,44 +1,32 @@
-window.setInterval('refresh()', 1000);
 
-const date = new Date();
-//*** 9-a
-const myDate = date.toLocaleDateString(),
-    time = date.toLocaleTimeString('ru');
+function clock(){
+    const date = new Date(),
+        myDate = date.toLocaleDateString(),
+        time = date.toLocaleTimeString('ru');
+    document.getElementById('clock').innerHTML = functionB(myDate) + "-" + functionB(time);
 
-const dateString = `${functionB(myDate)}-${functionB(time)}`;
-const p = document.createElement('p');
-p.innerHTML = dateString;
-document.body.appendChild(p);
+    const today = date.toLocaleString('ru', {
+            weekday: 'long',
+            month: 'long',
+            day: 'numeric',
+        }),
+        hour = date.getHours(),
+        minute = date.getMinutes(),
+        sec = date.getSeconds(),
+        year = date.toLocaleString('ru', {year: 'numeric'}),
+        hourStr = declNum(hour, ['час', 'часа', 'часов']),
+        minStr = declNum(minute, ['минута', 'минуты', 'минут']),
+        secStr = declNum(sec, ['секунда', 'секунды', 'секунд']);
 
-//*** 9-b
-const today = date.toLocaleString('ru', {
-        weekday: 'long',
-        month: 'long',
-        day: 'numeric',
-    }),
-    hour = date.getHours(),
-    minute = date.getMinutes(),
-    sec = date.getSeconds(),
-    year = date.toLocaleString('ru', {year: 'numeric'}),
-    hourStr = declNum(hour, ['час', 'часа', 'часов']),
-    minStr = declNum(minute, ['минута', 'минуты', 'минут']),
-    secStr = declNum(sec, ['секунда', 'секунды', 'секунд']);
-
-const formattedString = `Сегодня ${today} ${year} года ${hour} ${hourStr} ${minute} ${minStr} ${sec} ${secStr}`;
-const p1 = document.createElement('p');
-p1.innerHTML = formattedString;
-document.body.appendChild(p1);
-
-function refresh() {
-    window.location.reload();
+    document.getElementById('clock1').innerHTML = `Сегодня ${today} ${year} года ${hour} ${hourStr} ${minute} ${minStr} ${sec} ${secStr}`;
 }
 
+setInterval(clock, 1000);
 function declNum(n, str) {
     let cases = [2, 0, 1, 1, 1, 2];
     return str[(n % 100 > 4 && n % 100 < 20) ? 2 : cases[(n % 10 < 5) ? n % 10 : 5]];
 }
 
 function functionB(n) {
-    return (n > 0 && n < 10) ? '0' + n : n
+    return (n > 0 && n < 10) ? '0' + n : n;
 }
-
