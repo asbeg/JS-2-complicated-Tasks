4d70a7a8532a24c644c77086d7e9704c939ea485
BegaiAs <begaiasanbekova@gmail.com>
2021-02-23 18:27:30 +0100

    Lesson18 Animation

--- script/script.js
+++ script/script.js
@@ -1,32 +1,22 @@
+let zug = document.querySelector('.zug'),
+    btn = document.querySelector('#btn'),
+    animate = false,
+    count = 0,
+    interval;
 
-function clock(){
-    const date = new Date(),
-        myDate = date.toLocaleDateString(),
-        time = date.toLocaleTimeString('ru');
-    document.getElementById('clock').innerHTML = functionB(myDate) + "-" + functionB(time);
+let runAnimate = function () {
+    interval = requestAnimationFrame(runAnimate)
+    count++;
+    zug.style.left = count + 'px';
+};
 
-    const today = date.toLocaleString('ru', {
-            weekday: 'long',
-            month: 'long',
-            day: 'numeric',
-        }),
-        hour = date.getHours(),
-        minute = date.getMinutes(),
-        sec = date.getSeconds(),
-        year = date.toLocaleString('ru', {year: 'numeric'}),
-        hourStr = declNum(hour, ['час', 'часа', 'часов']),
-        minStr = declNum(minute, ['минута', 'минуты', 'минут']),
-        secStr = declNum(sec, ['секунда', 'секунды', 'секунд']);
+btn.addEventListener('click', function () {
+    if (!animate) {
+        interval = requestAnimationFrame(runAnimate);
+        animate = true;
+    } else {
+        animate = false;
+        cancelAnimationFrame(interval);
+    }
+});
 
-    document.getElementById('clock1').innerHTML = `Сегодня ${today} ${year} года ${hour} ${hourStr} ${minute} ${minStr} ${sec} ${secStr}`;
-}
-
-setInterval(clock, 1000);
-function declNum(n, str) {
-    let cases = [2, 0, 1, 1, 1, 2];
-    return str[(n % 100 > 4 && n % 100 < 20) ? 2 : cases[(n % 10 < 5) ? n % 10 : 5]];
-}
-
-function functionB(n) {
-    return (n > 0 && n < 10) ? '0' + n : n;
-}
